90be6007cacdbdd7528d5d4b48ce7bf9ed1cb839
Justyna Wojtczak <justine84@gmail.com>
2025-06-14 21:33:07 +0300

    Improve nodoc detection

--- lib/no_comments/comment_detector.rb
+++ lib/no_comments/comment_detector.rb
@@ -5,7 +5,7 @@ module NoComments
     MAGIC_COMMENT_REGEX = /\A#.*\b(?:frozen_string_literal|encoding|coding|warn_indent|fileencoding)\b.*\z/
     TOOL_COMMENT_REGEX = /\A#\s*(?:rubocop|reek|simplecov|coveralls|pry|byebug|noinspection|sorbet|type)\b/
     DOC_COMMENT_REGEX = /\A#\s*@\w+/
-    NODOC_REGEX = /#.*:nodoc:/
+    NODOC_STRING = ":nodoc:"
     CLASS_OR_MODULE_REGEX = /\A(?:class|module)\b/
     def magic_comment?(stripped_line)
       stripped_line.match?(MAGIC_COMMENT_REGEX)
@@ -17,7 +17,7 @@ module NoComments
 
     def documentation_comment?(stripped_line, next_line = nil)
       return true if stripped_line.match?(DOC_COMMENT_REGEX)
-      return true if stripped_line.match?(NODOC_REGEX)
+      return true if stripped_line.include?(NODOC_STRING)
       return true if next_line&.match?(CLASS_OR_MODULE_REGEX)
 
       false
